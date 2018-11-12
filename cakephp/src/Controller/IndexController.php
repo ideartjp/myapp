7db6956ba3fbf3581219f82c0b48596e1d5fc64b
ideart <ideart.jp@gmail.com>
2018-11-13 00:11:18 +0900

    new!

---
+++ cakephp/src/Controller/IndexController.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+use Cake\Core\Configure;
+
+class IndexController extends AppController
+{
+    public function input()
+    {
+        $prefecture = Configure::read('prefecture');
+        $checkin    = Configure::read('checkin');
+
+        $this->set('prefecture', $prefecture);
+        $this->set('checkin', $checkin);
+    }
+
+    public function confirm()
+    {
+        $data = $this->request->getData();
+        
+        $this->set('data', $data);
+    }
+
+    public function register()
+    {
+
+    }
+
+    public function complete()
+    {
+    }
+}
