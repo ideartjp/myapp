e6bb76071888848186161987c7dc38c25b94cacb
ideart <ideart.jp@gmail.com>
2018-11-14 23:29:20 +0900

    UPDATE（2018.11.14 23:30）

--- cakephp/src/Controller/IndexController.php
+++ cakephp/src/Controller/IndexController.php
@@ -3,6 +3,7 @@ namespace App\Controller;
 
 use App\Controller\AppController;
 use Cake\Core\Configure;
+use Cake\ORM\TableRegistry;
 
 class IndexController extends AppController
 {
@@ -18,13 +19,30 @@ class IndexController extends AppController
     public function confirm()
     {
         $data = $this->request->getData();
-        
+        $prefecture_name = Configure::read('prefecture.' . $this->request->getData('prefecture'));
+
+        $this->request->getSession()->write('data', $data);
+
+        $this->set('prefecture_name', $prefecture_name);
         $this->set('data', $data);
     }
 
     public function register()
     {
+        $data = $this->request->getSession()->read('data');
 
+        // モデルを呼び出す
+        $usersTable = TableRegistry::get('Users');
+
+        // エンティティクラスを作成
+        $users = $usersTable->newEntity();
+        // 登録するデータをプロパティに入れる
+        $users->family_name = $data['familyName'];
+
+        // データ保存
+        $usersTable->save($users);
+
+        exit;
     }
 
     public function complete()
